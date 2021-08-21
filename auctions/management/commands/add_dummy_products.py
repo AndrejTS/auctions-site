3233f940120599ca82dd957ca9fa0f3e6a615428
Andrej <70228739+AndrejTS@users.noreply.github.com>
2021-08-21 22:09:16 +0200

    move add_dummy_products script to management/commands folder and add timing for adding new product

---
+++ auctions/management/commands/add_dummy_products.py
@@ -0,0 +1,36 @@
+from django.core.management.base import BaseCommand, CommandError
+from auctions.models import Listing
+from auctions.models import User
+
+import json
+import random
+import time
+
+
+class Command(BaseCommand):
+    help = 'Add dummy products'
+
+    def handle(self, *args, **options):
+        with open('fake_products.json') as f:
+            products = json.load(f)
+
+        random.shuffle(products)
+
+        usernames = ['andrej', 'phiLip', 'mary', 'john']
+        users = []
+
+        for username in usernames:
+            try:
+                user = User.objects.get(username=username)
+                users.append(user)
+            except User.DoesNotExist:
+                raise CommandError('User "%s" does not exist' % username)
+
+        categories = ['Auto', 'Fashion', 'Electronics',
+                      'Home', 'Books', 'Sport', 'Other']
+
+        for listing in products:
+            time.sleep(random.randint(5, 10))
+            listing = Listing(title=listing['title'], starting_price=listing['price'], current_price=listing['price'],
+                              description=listing['description'], image_url=listing['image'], category=listing['category'], owner=random.choice(users))
+            listing.save()
